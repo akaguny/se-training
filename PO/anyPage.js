d2304fa4623bc4a52751da8952a9c1a5ebc3b07f
Alexey Shcherbakov <alexeykolpino@gmail.com>
2016-11-29 23:53:11 +0300

    Создал класс, в котором будут методы, подходящие к любой или почти любой странице

---
+++ PO/anyPage.js
@@ -0,0 +1,33 @@
+var _wdPromise = require('./../utils/wdPromise.js'),
+    promise = new _wdPromise;
+
+/**
+ * Конструктор
+ */
+function AnyPage() {
+};
+
+/**
+ * Получить текст h1
+ * @return {Promise.<String>} текст h1
+ */
+AnyPage.prototype.getH1TitleText = function() {
+  var h1,
+      text;
+
+  return wd.driver.findElements(wd.by.css('h1')).then(function (_h1) {
+    h1 = _h1;
+  }).then(function () {
+    if (h1.length > 0){
+      text = h1[0].getText().then(function (_text) {
+        return _text;
+      });
+    } else {
+      text = promise.anyToPromise(undefined);
+    }
+  }).then(function () {
+    return text;
+  });
+};
+
+module.exports = AnyPage;
